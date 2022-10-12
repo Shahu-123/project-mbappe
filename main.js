cecb1c6fa98da9426c08c49b86ff812f0523d799
Shahu-123 <67902097+Shahu-123@users.noreply.github.com>
2022-10-12 22:47:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -172,10 +172,7 @@ function accelerate(n) {
 }
 function whenclick() {
     if (myGamePiece.image.src == ("https://shahu-123.github.io/" + mbappeImage)) {
-	    myGamePiece.image.src = onmbappeImage;
-    }
-    if (myGamePiece.image.src == ("https://shahu-123.github.io/" + onmbappeImage)) {
-	    myGamePiece.image.src = mbappeImage;    
+	    myGamePiece.image.src = "https://shahu-123.github.io/onmbappeImage.tiff";
     }
     if (myGamePiece.image.src == ("https://shahu-123.github.io/" + playerImage)) {
             myGamePiece.image.src = onplayerImage;    
